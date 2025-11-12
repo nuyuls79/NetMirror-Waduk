f90a83672cda961a6d5ae8c40ed178fb55c99759
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2025-11-12 14:36:55 +0545

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "CNCVerse"
-
-// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
-
-
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-include("CNC Verse")
